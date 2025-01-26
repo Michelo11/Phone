fe156cfd9188d741f5ba21455f7b329109eebffb
Michelo11 <torre.michele07@gmail.com>
2025-01-26 15:45:55 +0000

    Updated resourcepack

--- src/main/java/me/michelemanna/phone/listeners/PlayerListener.java
+++ src/main/java/me/michelemanna/phone/listeners/PlayerListener.java
@@ -93,6 +93,6 @@ public class PlayerListener implements Listener {
             return;
         }
 
-        event.getPlayer().setResourcePack(PhonePlugin.getInstance().getConfig().getString("resourcepack.url", "https://github.com/Michelo11/Phone/releases/download/v1.0/Custom.Phone.zip"));
+        event.getPlayer().setResourcePack(PhonePlugin.getInstance().getConfig().getString("resourcepack.url", "https://github.com/Michelo11/Phone/releases/download/v1.10.4/Custom.Phone.zip"));
     }
 }
